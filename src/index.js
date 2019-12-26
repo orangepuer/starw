ccc172412f1a2ceb952c491155357d3a0b371e7d
orangepuer <rkhpuer@gmail.com>
2019-12-26 11:02:07 +0300

    Implement SwapiService class for api

---
+++ src/index.js
@@ -0,0 +1,50 @@
+class SwapiService {
+
+  _apiBase = 'https://swapi.co/api';
+
+  async getResourcre(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, received ${res.status}`)
+    }
+
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResourcre(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResourcre(`/people/${id}/`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResourcre(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResourcre(`/planets/${id}/`);
+  }
+
+  async getAllStarhips() {
+    const res = await this.getResourcre(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResourcre(`/starships/${id}/`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllStarhips().then((starships) => {
+  starships.forEach((starship) => {
+    console.log(starship.name);
+  });
+});
