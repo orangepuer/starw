0b79be9f36a1d85e0457acf6933ffce5c8abb075
orangepuer <rkhpuer@gmail.com>
2020-01-16 15:30:47 +0300

    Implement the ability to get a list of people

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,7 +8,8 @@ import './app.css';
 
 export default class App extends Component {
   state = {
-    showRandomPlanet: true
+    showRandomPlanet: true,
+    selectedPerson: null
   };
 
   toggleRandomPlanet = () => {
@@ -16,6 +17,12 @@ export default class App extends Component {
       return {
         showRandomPlanet: !state.showRandomPlanet
       }
+    })
+  };
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id
     })
   };
 
@@ -32,10 +39,10 @@ export default class App extends Component {
         </button>
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList />
+            <ItemList onItemSelected={this.onPersonSelected} />
           </div>
           <div className="col-md-6">
-            <PersonDetails />
+            <PersonDetails personId={this.state.selectedPerson} />
           </div>
         </div>
       </div>
