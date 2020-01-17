a26fd7be03bd5c73cb35800ba1f5d711455840b0
orangepuer <rkhpuer@gmail.com>
2020-01-17 13:44:56 +0300

    Implement the display of information about a person

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,25 +1,61 @@
 import React, { Component } from "react";
 import './person-details.css'
+import SwapiService from "../../services/swapi-service";
 
 export default class PersonDetails extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    person: null
+  };
+
+  updatePerson() {
+    const { personId } = this.props;
+
+    if (!personId) {
+      return;
+    }
+
+    this.swapiService.getPerson(personId).then((person) => {
+      this.setState({
+        person
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.updatePerson();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.personId !== this.props.personId) {
+      this.updatePerson();
+    }
+  }
+
   render() {
+    if (!this.state.person) {
+      return <span>Select a person from a list</span>
+    }
+    const { id, name, gender, birthYear, eyeColor } = this.state.person;
+
     return (
       <div className="person-details card">
-        <img src="https://starwars-visualguide.com/assets/img/characters/3.jpg" alt="" className="person-image"/>
+        <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} alt="" className="person-image"/>
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{eyeColor}</span>
             </li>
           </ul>
         </div>
