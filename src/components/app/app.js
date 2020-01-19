6047248ede79fccbfd3900affdd1dd9082b7fae5
orangepuer <rkhpuer@gmail.com>
2020-01-19 10:48:02 +0300

    Add PeoplePage component and implement Error boundaries

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,11 +7,11 @@ import PersonDetails from "../person-details";
 import './app.css';
 import ErrorButton from "../error-button";
 import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
 
 export default class App extends Component {
   state = {
     showRandomPlanet: true,
-    selectedPerson: null,
     hasError: false
   };
 
@@ -20,12 +20,6 @@ export default class App extends Component {
       return {
         showRandomPlanet: !state.showRandomPlanet
       }
-    })
-  };
-
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
     })
   };
 
@@ -53,14 +47,7 @@ export default class App extends Component {
           </button>
           <ErrorButton />
         </div>
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   }
--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './person-details.css'
 import SwapiService from "../../services/swapi-service";
+import ErrorButton from "../error-button";
 
 export default class PersonDetails extends Component {
   swapiService = new SwapiService();
@@ -58,6 +59,7 @@ export default class PersonDetails extends Component {
               <span>{eyeColor}</span>
             </li>
           </ul>
+          <ErrorButton />
         </div>
       </div>
     )
